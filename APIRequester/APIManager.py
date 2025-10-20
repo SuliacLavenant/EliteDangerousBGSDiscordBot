a2a1977359201b2764f85d40013b4edd8a07f752
SuliacLavenant <suliac.lavenant@gmail.com>
2025-10-20 16:13:11 +0200

    use of API Manager

---
+++ APIRequester/APIManager.py
@@ -0,0 +1,18 @@
+
+
+from APIRequester.EliteBGSAPIAPIRequester import EliteBGSAPIAPIRequester
+from APIRequester.EDSMAPIRequester import EDSMAPIRequester
+
+from DataClass.System import System
+
+# TODO check different api to get the most up to date data
+class APIManager:
+
+    def requestSystemData(systemName: str):
+        return EDSMAPIRequester.requestSystemData(systemName)
+
+    def requestMinorFactionBaseInformation(minorFactionName: str):
+        return EliteBGSAPIAPIRequester.requestMinorFactionBaseInformation(minorFactionName)
+
+    def requestMinorFactionSystemData(system: System):
+        return EDSMAPIRequester.requestMinorFactionSystemData(system)
