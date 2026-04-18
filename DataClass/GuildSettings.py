5e8372c9da366ef7992e86720bf204d534a419e5
Suliac Lavenant <suliac.lavenant@gmail.com>
2026-04-19 00:03:58 +0200

    add guild id to guild settings

--- DataClass/GuildSettings.py
+++ DataClass/GuildSettings.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass, field
 
 @dataclass
 class GuildSettings:
+    guild_id: int = None
+
     minor_faction_name: str = None
 
     #channels
@@ -15,6 +17,7 @@ class GuildSettings:
     @classmethod
     def init_from_dict(cls, guild_settings_dict: dict):
         guild_settings = cls(
+            guild_id=guild_settings_dict["guild_id"],
             minor_faction_name=guild_settings_dict["minor_faction_name"],
             bgs_change_log_channel_id=guild_settings_dict["bgs_change_log_channel_id"],
             bgs_system_recap_channel_id=guild_settings_dict["bgs_system_recap_channel_id"],
@@ -27,6 +30,7 @@ class GuildSettings:
 
     def get_as_dict(self) -> dict:
         guild_settings_dict = {}
+        guild_settings_dict["guild_id"] = self.guild_id
         guild_settings_dict["minor_faction_name"] = self.minor_faction_name
         guild_settings_dict["bgs_change_log_channel_id"] = self.bgs_change_log_channel_id
         guild_settings_dict["bgs_system_recap_channel_id"] = self.bgs_system_recap_channel_id
